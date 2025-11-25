33d1499a81c90767cac8e0cd2aca405cde1195d1
abemasaton <j.f.ma.0620@gmail.com>
2025-11-25 11:40:52 +0900

    クリック時の動作を整理　地雷クリック時にメッセージを表示

--- orizinal_mineseeeper/orizinal_mineseeeper/create_field.cs
+++ orizinal_mineseeeper/orizinal_mineseeeper/create_field.cs
@@ -34,6 +34,10 @@ namespace orizinal_mineseeeper
 
         private int modeflag; // 0:開ける　1:旗　2:一マス開け　3:縦一列開け　4:横一行開け
 
+        private bool Openedmas = false; // マスが開いているかの確認
+
+        private bool flagedflag = false; // 旗が立っているかの確認
+
 
         public create_field (Form1 Form1,int FieldSize,int _tate,int _yoko, bool onmine)
         {
@@ -57,35 +61,41 @@ namespace orizinal_mineseeeper
             BackColor = _CloseColor;
             // 地雷の設定
             mineflag = onmine;
-            // 旗が立っているかの確認
 
 
             Click += ClickEvent;
 
         }
-        public void Openfield() // 共通のオープン処理
+        public int Openfield()
         {
+            int Cnt = 0;
+            for (int i = 0; i < _CheckData.Length; i++)
+            {
+                var data = _CheckData[i];
+                var button = _Form1.Getfieldbutton(tate + data[0], yoko + data[1]);
+
+                if (button != null && button.mineflag)
+                {
+                    Cnt++;
+                }
+            }
+            Text = Cnt.ToString();
+            return Cnt;
+        }
+        public void mineCheck() // 共通のオープン処理
+        {
+            if(Openedmas) MessageBox.Show("ますはひｒかれた");
             if (mineflag)
             {
                 Text = ("M");
+                MessageBox.Show("あなたは地雷を踏みました lol");
             }
             else
             {
-                int Cnt = 0;
-                for (int i = 0; i < _CheckData.Length; i++)
-                {
-                    var data = _CheckData[i];
-                    var button = _Form1.Getfieldbutton(tate + data[0], yoko + data[1]);
-
-                    if (button != null && button.mineflag)
-                    {
-                        Cnt++;
-                    }
-                }
-                Text = Cnt.ToString();
+                Openfield();
             }
             BackColor = _OpenColor;
-
+            Openedmas = true;
             this.Click -= ClickEvent; // クリックイベントの削除
         }
 
@@ -106,7 +116,7 @@ namespace orizinal_mineseeeper
             modeflag = Form1.ReturnMode();
             if (modeflag == 0) // モードオープン
             {
-                _Form1.Getfieldbutton(tate, yoko).Openfield();
+                _Form1.Getfieldbutton(tate, yoko).mineCheck();
             }
         }
     }
